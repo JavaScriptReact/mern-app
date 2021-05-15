68cd9c0eb7ff186263df51b1c19f1949f6a84da3
JavaScriptReact <nevim@seznam.cz>
2021-05-15 22:08:07 +0200

    commited with update

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDom from "react-dom";
+import axios from "axios";
 
 import "./styles/styl/style.css";
 
 function Application() {
+  const [value, setValue] = useState("");
+
+  const send = () => {
+    axios
+      .post("/api/usernames", { value: value })
+      .then(() => setValue(""))
+      .catch((error) => alert(error));
+  };
+
   return (
     <section>
-      <h1>This web has created by MERN.</h1>
+      <label htmlFor="input">Username : </label>
+      <input
+        type="text"
+        id="input"
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+      />
+      <button type="button" onClick={send}>
+        Send
+      </button>
+      <a href="/api/users">Application API</a>
     </section>
   );
 }
