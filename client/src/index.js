0ed3caef825873275d1e6ff90bbb9eae897bf961
JavaScriptReact <nevim@seznam.cz>
2021-05-15 20:57:14 +0200

    update

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+import "./styles/styl/style.css";
+
 function Application() {
   return (
-    <>
+    <section>
       <h1>This web has created by MERN.</h1>
-    </>
+    </section>
   );
 }
 
