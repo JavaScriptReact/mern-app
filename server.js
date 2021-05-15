4582ca0789d45ba9123561179c13e0efb891974e
JavaScriptReact <nevim@seznam.cz>
2021-05-15 20:43:25 +0200

    update

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 
+const app = express();
+
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "client", "build")));
 
